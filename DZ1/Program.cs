1975c9782574ab092fb72b2498eedbdfdb93084b
Nimere1990 <nimereiv@gmail.com>
2022-10-07 17:08:02 +0300

    Создание

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -4,3 +4,31 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3
 */
+
+Console.Write($"Сколько чисел вы хотетие ввести? ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] masNum = new int[m];
+
+void InputNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        masNum[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+int Comparison(int[] masNum)
+{
+    int count = 0;
+    for (int i = 0; i < masNum.Length; i++)
+    {
+        if (masNum[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше нуля: {Comparison(masNum)} ");
